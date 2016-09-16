bcf8fcd7036092103af8cd46fd682c5887b83cdb
Thomas VB <van.bouwel.thomas@gmail.com>
2016-09-16 16:35:52 +0200

    First Commit

---
+++ Mobilityjam/Assets/ViveGrip/ViveGrip/Examples/Scripts/ViveGripExample_ExtendHighlight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// See EXTENSIONS.md for more information
+
+public class ViveGripExample_ExtendHighlight : MonoBehaviour {
+  private Renderer objectRenderer;
+  private Color baseColor;
+
+  void Start() {
+    objectRenderer = GameObject.Find("Right Hand").GetComponent<Renderer>();
+    baseColor = objectRenderer.material.color;
+  }
+
+  void ViveGripHighlightStart() {
+    if (!this.enabled) { return; }
+    objectRenderer.material.color = Color.Lerp(baseColor, Color.white, 0.5f);
+  }
+
+  void ViveGripHighlightStop() {
+    if (!this.enabled) { return; }
+    objectRenderer.material.color = baseColor;
+  }
+}
